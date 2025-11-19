1d993f60f6d83686464101fcd81b008549716f32
Josue Enmanuel Poot Mateo <josue.poot@tecdesoftware.edu.mx>
2025-11-19 15:40:27 -0600

    Agrega estructura base del sitio web

---
+++ js/script.js
@@ -0,0 +1,10 @@
+console.log("Sitio cargado correctamente 🍀");
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.querySelector("form");
+    if (form) {
+        form.addEventListener("submit", function(event) {
+            event.preventDefault();
+            alert("¡Gracias por tu mensaje!");
+        });
+    }
+});
